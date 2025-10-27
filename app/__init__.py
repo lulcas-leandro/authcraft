2754c7d304c2599578ac778e445d89cae01ef4d7
unknown <lucasleandro.cdev@gmail.com>
2025-10-27 09:21:33 -0300

    feat: add configuração do banco de dados e modelo User

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    @app.route('/main')
-    def main_test():
-        return "<h1>Main funcionando!</h1>"
+    db.init_app(app)
+    migrate.init_app(app, db)
     
     return app
