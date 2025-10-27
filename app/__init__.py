6f1ffa131b985ac5cf4bd620f6209e760ceb97da
unknown <lucasleandro.cdev@gmail.com>
2025-10-27 09:39:38 -0300

    feat: Configura o gerenciamento de sessão com flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,13 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.login_message = 'Por favor, faça login para acessar esta página'
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -12,5 +16,6 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
     
     return app
